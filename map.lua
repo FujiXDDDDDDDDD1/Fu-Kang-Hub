f850e1898c3d23ef2d9ddd1c7b8bc0f81ccb79d9
FujiXDDDDDDDDD1 <71167709+FujiXDDDDDDDDD1@users.noreply.github.com>
2023-05-12 12:23:35 +0700

    Update map.lua

--- map.lua
+++ map.lua
@@ -1,1 +1,11 @@
-
+if PlaceId == 3351674303 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FujiXDDDDDDDDD1/Fu-Kang-Hub/main/Driving%20Empire"))()
+    
+    elseif PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FujiXDDDDDDDDD1/Fu-Kang-Hub/main/Da%20Hood%20(Aimbot)"))()
+    
+        else
+            game.Players.LocalPlayer:Kick("ควย อ้วน")
+            wait(1)
+            game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+    end
